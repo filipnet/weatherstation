4b86ea4a1a1085adade2b8c385858363b216634d
filipnet <63908077+filipnet@users.noreply.github.com>
2022-09-11 17:08:57 +0200

    Update config.h

--- src/config.h
+++ src/config.h
@@ -1,5 +1,7 @@
 #define RELAY_PUMP D1
 #define RELAY_SOCKET D2
+#define SDA_PIN D2 // dataPin
+#define SCL_PIN D1 // clockPin
 #define REED_PIN D3 // internal 10k pull-up
 // D4 BUILTIN_LED so do not use, internal 10k pull-up
 #define HEARTBEAT_INTERVALL 512000
